cd27d286dda274a9b5b213ba63e2e90897dc6bf0
cceballosq <cceballosq@gmail.com>
2013-10-09 23:49:11 -0500

    Update practica10.c
    modificacion

--- practica1/practica10.c
+++ practica1/practica10.c
@@ -1,38 +1,40 @@
-#include<16F877A.h>
+#include<16F887A.h>
 #device adc=10
 #use delay (clock = 4000000)
-#include <lcd.c> // driver para usar la lcd
-#include <math.h>
 #include <stdlib.h> //libreia para funciones como read_adc y otras
 #fuses XT,NOWDT,PUT,NOLVP,NOBROWNOUT
-float cif=0,aux=0,cif2=0,aux2=0;
+int valor;
 
 void main ()
 {
 
-set_tris_a(0xff);
-set_tris_d(0x00);
+set_tris_a(0xff);//se pone el puerto RA analogo de entrada
+set_tris_d(0x00);//se pone el puerto RD en salida para prender los led
 
-setup_adc_ports(ALL_ANALOG);
-setup_adc(ADC_CLOCK_DIV_32);
+setup_adc_ports(ALL_ANALOG);//configuracion de los puertos analogos
+setup_adc(ADC_CLOCK_DIV_32);//el osilador del ADC
 
-lcd_init();
 while (true)
 {
-delay_ms(500);
+
 set_adc_channel(0);
 delay_us(10);
-cif = read_adc();
-cif *= (float).488281;    
-lcd_gotoxy(1,1);
-printf(lcd_putc,"temp 1: %2.2f",cif);
+valor = read_adc();
+if(valor){
+  output_d(0x01);
+}else{
+  output_d(0x00);
+}
 
 set_adc_channel(1);
 delay_us(10);
-cif2 = read_adc();
-cif2 *= (float).488281; 
-lcd_gotoxy(1,2);
-printf(lcd_putc,"temp 2: %2.2f",cif2);
+valor = read_adc();
+if{
+  output_d(0x02);
+}else{
+  output_d(0x00);
+}
+
 
 
 
