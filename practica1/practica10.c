9225bf619b2a7cd7a74a9ee35a5f841c2e991210
cceballosq <cceballosq@gmail.com>
2013-10-09 23:51:32 -0500

    acomodo de las lineas

--- practica1/practica10.c
+++ practica1/practica10.c
@@ -7,39 +7,32 @@ int valor;
 
 void main ()
 {
+  set_tris_a(0xff);//se pone el puerto RA analogo de entrada
+  set_tris_d(0x00);//se pone el puerto RD en salida para prender los led
 
-set_tris_a(0xff);//se pone el puerto RA analogo de entrada
-set_tris_d(0x00);//se pone el puerto RD en salida para prender los led
+  setup_adc_ports(ALL_ANALOG);//configuracion de los puertos analogos
+  setup_adc(ADC_CLOCK_DIV_32);//el osilador del ADC
 
-setup_adc_ports(ALL_ANALOG);//configuracion de los puertos analogos
-setup_adc(ADC_CLOCK_DIV_32);//el osilador del ADC
+  while (true)
+  {
 
-while (true)
-{
+    set_adc_channel(0);
+    delay_us(10);
+    valor = read_adc();
+    if(valor){
+      output_d(0x01);
+    }else{
+      output_d(0x00);
+    }
 
-set_adc_channel(0);
-delay_us(10);
-valor = read_adc();
-if(valor){
-  output_d(0x01);
-}else{
-  output_d(0x00);
-}
-
-set_adc_channel(1);
-delay_us(10);
-valor = read_adc();
-if{
-  output_d(0x02);
-}else{
-  output_d(0x00);
-}
-
-
-
-
-
-}
-
+    set_adc_channel(1);
+    delay_us(10);
+    valor = read_adc();
+    if{
+      output_d(0x02);
+    }else{
+      output_d(0x00);
+    }
+  }
 }
 
